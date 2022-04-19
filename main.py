f3a74cfa1eacc367ea8a826fbdf95931ed20740b
Ethgreninja2210 <97811423+Ethgreninja2210@users.noreply.github.com>
2022-04-19 10:07:23 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,33 @@
+import pyautogui
+import time
+
+while True:
+    time.sleep(4)
+    pyautogui.typewrite("Hello This is an automated message.")
+    time.sleep(2)
+    pyautogui.press('enter')
+
+while True:
+    pyautogui.typewrite("Never Gonna Give you up")
+    time.sleep(2)
+    pyautogui.press("enter")
+
+    pyautogui.typewrite("Never gonna let you down")
+    time.sleep(2)
+    pyautogui.press("enter")
+
+    pyautogui.typewrite("Never gonna run around and desert you")
+    time.sleep(2)
+    pyautogui.press("enter")
+
+    pyautogui.typewrite("Never gonna make you cry")
+    time.sleep(2)
+    pyautogui.press("enter")
+
+    pyautogui.typewrite("Never gonna say goodbye")
+    time.sleep(2)
+    pyautogui.press("enter")
+
+    pyautogui.typewrite("Never gonna tell a lie and hurt you")
+    time.sleep(2)
+    pyautogui.press("enter")
